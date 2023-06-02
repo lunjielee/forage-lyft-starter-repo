5368d5d7657e5030b95519411245ab4df9b6cc35
Lunjie Li <lunjielee@qq.com>
2023-06-01 22:43:25 -0400

    Use the correct way to use class

--- car_factory.py
+++ car_factory.py
@@ -1,9 +1,9 @@
 from car import Car
-from engine import capulet_engine
-from engine import sternman_engine
-from engine import willoughby_engine
-from battery import nubbin_battery
-from battery import spindler_battery
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
 
 
 class CarFactory():
@@ -15,9 +15,9 @@ class CarFactory():
 
     def create_palindrome(current_date, last_service_date, warning_light_on):
         return Car(sternman_engine(warning_light_on), spindler_battery(current_date, last_service_date))
-    
+
     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
         return Car(willoughby_engine(current_mileage, last_service_mileage), nubbin_battery(current_date, last_service_date))
-    
+
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
         return Car(capulet_engine(current_mileage, last_service_mileage), nubbin_battery(current_date, last_service_date))
