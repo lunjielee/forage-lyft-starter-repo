874521767155ef0fd50ac2a799f3e10abeb74828
Lunjie Li <lunjielee@qq.com>
2023-06-01 23:00:38 -0400

    Spindler battery now last 3 years

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -7,4 +7,4 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date
 
     def needs_service(self):
-        return (self.last_service_date.replace(year=self.last_service_date.year + 2)) < self.current_date
+        return (self.last_service_date.replace(year=self.last_service_date.year + 3)) < self.current_date
